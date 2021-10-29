fbf134caec8782cd4f4969242f616ba728ceb9d2
Toggi <thorgilshjalmarsson@gmail.com>
2021-10-29 14:57:44 +0000

    Buyer service running with docker-compose with presistent db

--- BuyerService/buyersAPI.py
+++ BuyerService/buyersAPI.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends
 from container import Container
 from models.buyer_model import BuyerModel
 from buyer_repository import BuyerRepository
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
@@ -14,11 +15,12 @@ async def create_buyer(buyer: BuyerModel, BuyerRepo: BuyerRepository = Depends(
     return BuyerRepo.create_buyer(buyer)
 
 
-
 @router.get('/buyers/{id}', status_code=200)
 @inject
-async def get_buyer(id: int, BuyerRepo: BuyerRepository = Depends(Provide[Container.buyer_repository_provider])):
+async def get_buyer(id: int,
+                    BuyerRepo: BuyerRepository = Depends(
+                        Provide[Container.buyer_repository_provider])):
     buyer = BuyerRepo.get_buyer(id)
     if buyer is None:
-        status_code = 400
+        return JSONResponse(status_code=404)
     return BuyerRepo.get_buyer(id)
