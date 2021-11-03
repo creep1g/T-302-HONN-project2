2cac8702d01111bbeeea0dbdc2373db7568bf1b4
Toggi <thorgilshjalmarsson@gmail.com>
2021-11-03 14:35:49 +0000

    OrderDb fixed

--- OrderDb/order.sql
+++ OrderDb/order.sql
@@ -3,39 +3,26 @@ CREATE TABLE public.ORDER(
     product_id INT NOT NULL,
     merchant_id INT NOT NULL,
     buyer_id INT NOT NULL,
-    card_id INT NOT NULL,
-    discount FLOAT4 NOT NULL
+    card_number VARCHAR(20) NOT NULL,
+    discount FLOAT4 NOT NULL,
+	totalPrice FLOAT4 NOT NULL
 )
 TABLESPACE pg_default;
 
 ALTER TABLE public.ORDER
     OWNER TO postgres;
 
-CREATE TABLE public.CreditCard(
-    id SERIAL NOT NULL,
-    cardNumber VARCHAR(255) NOT NULL,
-    expirationMonth INT NOT NULL,
-    expirationDay INT NOT NULL,
-    cvc INT NOT NULL
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.CreditCard
-    OWNER TO postgres;
-
-
-INSERT INTO public.ORDER(
-    product_id,
-    merchant_id,
-    buyer_id,
-    card_id,
-    discount
-)
-values(
-    1,
-    1,
-    1,
-    1,
-    1
-)
+/* INSERT INTO public.ORDER( */
+/*     product_id, */
+/*     merchant_id, */
+/*     buyer_id, */
+/*     card_id, */
+/*     discount */
+/* ) */
+/* values( */
+/*     1, */
+/*     1, */
+/*     1, */
+/*     1, */
+/*     1 */
+/* ) */
