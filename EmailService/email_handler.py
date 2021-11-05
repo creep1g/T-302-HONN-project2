19ca5fdf5c90951e2c3bc665478403a9f6bf3ffb
Indiana Lif Bergsteinsdottir <indianalifb@Indianas-MacBook-Pro.local>
2021-11-05 02:50:20 +0000

    emailService

--- EmailService/email_handler.py
+++ EmailService/email_handler.py
@@ -3,8 +3,8 @@ import yagmail
 from email_config import EmailConfig
 
 
-class EmailHandler(EmailConfig):
-    def __init__(self, email_config: EmailConfig) -> None:
+class EmailHandler:
+    def __init__(self, email_config: EmailConfig):
         email = email_config.username
         pw = email_config.password
         self.yag = yagmail.SMTP(email, pw)
@@ -12,8 +12,12 @@ class EmailHandler(EmailConfig):
     def send_email(self, to, subject, contents):
         self.yag.send(to, subject, contents)
 
-    def order_created_email(self):
-        pass
+    def order_created_email(self, merchant_email, buyer_email, orderId, productName, totalPrice):
+        print("$$$$$$$$$$$$$ IN order_created_email &&&&&&&&&&&&&&&&&&&&")
+        to = [merchant_email, buyer_email]
+        subject = "Order has been created"
+        contents = [str(orderId), productName, str(totalPrice)]
+        self.send_email(to, subject, contents)
 
     def payment_successful_email(self):
         pass
--- EmailService/main.py
+++ EmailService/main.py
@@ -1,5 +1,82 @@
+from logging import Handler
+from email_config import EmailConfig
 from container import Container
+import pika
+from retry import retry
+from container import Container
+from Settings import Settings
+from email_handler import EmailHandler
+import json
+import requests
+from dependency_injector.wiring import Provide
+
+merchant_service = "http://merchant_service:8001/merchants"
+buyer_service = "http://buyer_service:8002/buyers"
+inventory_service = "http://inventory_service:8003/products"
+
+
+def arrange():
+    container = Container()
+    container.config.from_pydantic(Settings(_env_file='.env'))
+    return container
+
+
+@retry(pika.exceptions.AMQPConnectionError, delay=5, jitter=(1, 3))
+def get_connection():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='rabbit1', port=5672)
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue='paymentSuccess')  # Declare a queue
+    channel.queue_declare(queue='paymentFailure')  # Declare a queue
+    channel.queue_declare(queue='order')  # Declare a queue
+    return channel
+
+
+def callback_order(ch, method, properties, body):
+    print("IN callback_order")
+    body = body.decode("utf-8")
+    body = json.loads(body)
+
+    merchantId = (body["merchantId"])
+    print("merchantId: ", merchantId)
+    merchant = requests.get(f'''{merchant_service}/{merchantId}''').json()
+    print("merchant: ", merchant)
+    merchant_email = merchant["email"]
+    print("merchant_email: ", merchant_email)
+
+    buyerId = (body["buyerId"])
+    buyer = requests.get(f'''{buyer_service}/{buyerId}''').json()
+    buyer_email = buyer["email"]
+    print("buyer_email: ", buyer_email)
+    orderId = (body["orderId"])
+    totalPrice = (body["totalPrice"])
+    productId = (body["productId"])
+    product = requests.get(f'''{inventory_service}/{productId}''').json()
+    productName = product["productName"]
+    handler = container.email_handler_provider()
+    handler.order_created_email(
+        merchant_email, buyer_email, orderId, productName, totalPrice)
+    # Email body-ið ætti að innihalda id-ið á kaupunum, nafn-ið á vörunni og verðið á kaupunum
+
+
+def callback_payment_success(ch, method, properties, body):
+    print("IN callback_payment_success")
+    body = body.decode("utf-8")
+    body = json.loads(body)
+
+
+def callback_payment_failed(ch, method, properties, body):
+    print("IN callback_payment_failed")
+    body = body.decode("utf-8")
+    body = json.loads(body)
 
 
 if __name__ == '__main__':
-    container = Container()
+    container = arrange()
+    connection = get_connection()
+    print("IN email main")
+    connection.basic_consume(queue='order',
+                             on_message_callback=callback_order,
+                             auto_ack=True)
+    connection.start_consuming()
